188a78ef625ffdc98562f804c4437b0ad5812a27
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-09-11 17:03:04 +0100

    functions task

--- functions-starter-code/app.js
+++ functions-starter-code/app.js
@@ -1,26 +1,97 @@
 // 1. Write a function called 'multiply' that multiplies two numbers and returns the result
-function multiply () {
-
+function multiply (num1, num2) {
+  var result = num1*num2;
+  return result;
 }
 
 // 2. Write a function called 'addThree' that adds three numbers together and returns the result
+function addThree (num1, num2, num3) {
+  var result = num1+num2+num3;
+  return result;
+}
 
 // 3. Write a function called 'smallestNumber' that returns the smaller of 2 numbers
+function smallestNumber (num1, num2) {
+  if (num1<num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
 // 4. Write a function called 'maxOfThree' that returns the largest of 3 numbers
+function maxOfThree(num1, num2, num3) {
+  if (num1>num2 && num1>num3) {
+    return num1;
+  } else if (num2>num1 && num2>num3) {
+    return num2;
+  } else return num3;
+}
+
+// return Math.max(num1, num2, num3);
 
 // 5. Write a function called 'reverseString' that returns the reverse a string
+function reverseString(string) {
+  let reversed = "";
+  for (var i = string.length - 1; i >= 0; i--){
+    reversed += string[i]; //puts the last element first, second last element -> second, third last -> third etc.
+  }
+  return reversed;
+}
 
 // 6. Write a function called 'disemvowel' that returns a string with the vowels stripped out
+function disemvowel(string) {
+  var replace = string.replace(/[aeiou]/gi, ''); // [aeiou]: what we're searching for; g: search everything; i: case insensitive; second argument: what we're replacing with
+  return replace;
+}
 
 // 7. Write a function called 'removeOdd' that removes all ODD number from an array
+function removeOdd(array) {
+  // console.log(array);
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      array.splice(i, i + 1);
+    }
+    // console.log(array);
+  }
+  return array;
+}
 
 // 8. Write a function called 'removeEven' that removes all EVEN number from an array
+function removeEven(array) {
+  // console.log(array);
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      array.splice(i, i + 1);
+    }
+    // console.log(array);
+  }
+  return array;
+
+  // return array.filter(function(item)) {
+  //  return item % 2 !== 0;
+  //}
+}
 
 // 9. Write a function called 'longestString' that takes an array of strings and returns the string with the longest character length
+function longestString(array) {
+  var long = '';
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > long.length) {
+      long = array[i];
+    }
+  }
+  return long;
+}
 
 // 10. Write a function called 'allElementsExceptFirstThree' that discards the first 3 elements of an array,
 // e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
+function allElementsExceptFirstThree(array) {
+  for (var i = 0; i < 3; i++) {
+    array.shift();
+  }
+  return array;
+}
 
 //#### BONUS ####
 
